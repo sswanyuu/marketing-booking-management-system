13513aeac2664bd72174614f9041dfe90b941c3d
Olgaa <yosoywanyushih@gmail.com>
2025-09-22 22:41:36 +1000

    revert config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: './', // Use relative paths instead of absolute paths
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
   },
