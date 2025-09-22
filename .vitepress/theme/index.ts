2a501b23f842c5647dc03bd78488e34cb8cf01bf
Olgaa <yosoywanyushih@gmail.com>
2025-09-22 20:56:08 +1000

    update path and paranthesis

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,7 @@
-import DefaultTheme from "vitepress/theme";
+import DefaultTheme from 'vitepress/theme';
 
-import "./styles/custom.scss";
-import DefaultLayout from "../../src/layouts/DefaultLayout.vue";
+import './styles/custom.scss';
+import DefaultLayout from '../../src/layouts/DefaultLayout.vue';
 export default {
   ...DefaultTheme,
   Layout: DefaultLayout,
