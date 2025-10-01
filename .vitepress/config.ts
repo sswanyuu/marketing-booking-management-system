2ee27f573043a343c507c59e89285b271fd8c803
Olgaa <yosoywanyushih@gmail.com>
2025-10-02 08:49:20 +1000

    build

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: './',
+  // base: './',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
   },
